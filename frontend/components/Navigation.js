59bdf4bd5edafa952b50b3db87b370eb292790a4
tsamantanis <philip.tsamantanis@icloud.com>
2021-02-01 11:44:40 -0800

    Add new post page fe

--- frontend/components/Navigation.js
+++ frontend/components/Navigation.js
@@ -7,9 +7,7 @@ function Navigation() {
               <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+            <a href="/posts/new" className="btn btn-outline-primary ml-1 navbar-btn">New Post</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
---
+++ frontend/pages/posts/new.js
@@ -0,0 +1,80 @@
+import Layout from '../../components/Layout'
+import { useState } from 'react'
+import axios from 'axios'
+
+function NewPost() {
+    const [title, setTitle] = useState('')
+    const [summary, setSummary] = useState('')
+    const [error, setError] = useState('')
+    async function handleSubmit(event) {
+        event.preventDefault()
+        if (!title || title.length === 0 || !summary || summary.length === 0) {
+            setError('Please fill out all fields');
+            return
+        }
+        try {
+            const res = await axios.post(process.env.NEXT_APP_URI + '/posts/new', {
+                post: {
+                    title: title,
+                    summary: summary,
+                }
+            })
+        } catch (error) {
+            setError(error.message);
+        }
+    }
+    return (
+        <Layout>
+            <div className="container">
+                <div className="row justify-content-center align-items-center">
+                    <div className="col-12 col-sm-8 col-md-6 col-lg-4">
+                        <form onSubmit={ handleSubmit } method="post">
+                            <legend>New Post</legend>
+                            <div className="form-group">
+                                <label htmlFor="post-title">Title</label>
+                                <input
+                                    type="text"
+                                    name="title"
+                                    className="form-control"
+                                    id="post-title"
+                                    placeholder="Title"
+                                    value={title}
+                                    onChange={(event) => {
+                                        setTitle(event.target.value);
+                                        setError('')
+                                    }}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="post-summary">Summary</label>
+                                <textarea
+                                    name="summary"
+                                    className="form-control"
+                                    id="post-summary"
+                                    placeholder="Summary"
+                                    value={summary}
+                                    onChange={(event) => {
+                                        setSummary(event.target.value)
+                                        setError('')
+                                    }}
+                                ></textarea>
+                            </div>
+                            { error && error.length > 0 &&
+                                <div className="form-group text-center">
+                                    <small className="text-danger">
+                                        {error}
+                                    </small> 
+                                </div>
+                            }
+                            <div className='text-center mt-2'>
+                                <button type="submit" className="btn btn-primary">Create Post</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default NewPost;
