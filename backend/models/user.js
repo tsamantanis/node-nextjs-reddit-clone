775689d9c38940cba6720c68bde418b735307eea
tsamantanis <philip.tsamantanis@icloud.com>
2021-02-06 10:39:10 -0800

    Create hashed passwords with bcryptjs

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,31 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
     password: { type: String, select: false },
-    username: { type: String, required: true }
+    username: { type: String, required: true, unique: true }
 },
   {timestamps: true}
 );
 
-module.exports = mongoose.model("User", UserSchema);
+UserSchema.pre("save", function (next) {
+    const user = this
+    if (!user.isModified("password")) {
+        return next()
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            user.password = hash
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function (password, done) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        done(err, isMatch)
+    });
+};
+
+module.exports = mongoose.model("User", UserSchema)
