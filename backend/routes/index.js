5002682079dc5a116194c56987d40f2b975fe189
tsamantanis <philip.tsamantanis@icloud.com>
2021-02-10 15:54:41 -0800

    Create checkAuth function

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
-router.use('/posts', require('./posts'));
-router.use('/users', require('./users'));
+router.use('/posts', require('./posts'))
+router.use('/users', require('./users'))
 router.get('/', function(req, res) {
     return res.send("success")
 })
--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,22 @@ require('./data/reddit-db')
 
 const app = express()
 
+const checkAuth = (req, res, next) => {
+    if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+        req.user = null;
+    } else {
+        const token = req.cookies.nToken;
+        const decodedToken = jwt.decode(token, { complete: true }) || {};
+        req.user = decodedToken.payload;
+    }
+    next();
+}
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-
+app.use(checkAuth)
 // routes
 app.use(require('./routes'))
 
