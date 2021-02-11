e48688fd46eaa762d4f9b6694b3e2f8abdd8430d
tsamantanis <philip.tsamantanis@icloud.com>
2021-02-10 16:03:09 -0800

    Hide new post button when user is not logged in

--- frontend/components/PanelRight.js
+++ frontend/components/PanelRight.js
@@ -1,16 +1,21 @@
 import Link from 'next/link'
+import { useCookies } from "react-cookie"
 import styles from '../styles/Layout.module.css'
 
 function PanelRight() {
-    return (
-        <div className="d-none d-md-block col-md-2">
-            <Link href="/posts/new">
-                <div className={styles.newPost}>
-                    <span>New Post</span>
-                </div>
-            </Link>
-        </div>
-    )
+    const [cookie, setCookie] = useCookies(["user"])
+    console.log(cookie)
+    if (cookie && cookie.user)
+        return (
+            <div className="d-none d-md-block col-md-2">
+                <Link href="/posts/new">
+                    <div className={styles.newPost}>
+                        <span>New Post</span>
+                    </div>
+                </Link>
+            </div>
+        )
+    return null
 }
 
 export default PanelRight
