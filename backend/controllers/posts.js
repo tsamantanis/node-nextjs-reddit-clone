b0879385a7f18dc78f1c242629383ba217f8f52a
tsamantanis <philip.tsamantanis@icloud.com>
2021-02-13 07:21:51 -0800

    User posts association

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose")
 const Post = require('../models/post')
-
+const User = require('../models/user');
 const PostController = {}
 
 PostController.newPost = (req, res) => {
     if (req.user) {
         const post = new Post(req.body)
-        post.save((err, post) => {
-            if (err) return res.status(500).json({ message: err.message })
-            return res.json({ message: "Post created successfully"})
-        })
+        post.author = req.user._id
+        post.save().then(post => {
+                return User.findById(req.user._id);
+            }).then(user => {
+                user.posts.unshift(post);
+                user.save();
+                // SHOULD REDIRECT TO THE NEW POST
+                return res.json({ message: "Post created successfully"})
+            }).catch(err => {
+                return res.status(500).json({ message: err.message })
+            });
     } else return res.status(401).json({message: "Unauthorized"})
 }
 
