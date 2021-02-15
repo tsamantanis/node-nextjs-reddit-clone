62693a9848ee8a375f60b6a8fe96a2766fd2dd02
tsamantanis <philip.tsamantanis@icloud.com>
2021-02-15 14:04:56 -0800

    Update posts test to accommodate user validation

--- backend/tests/posts.test.js
+++ backend/tests/posts.test.js
@@ -7,6 +7,7 @@ const expect = chai.expect;
 // Import the Post model from our models folder so we
 // we can use it in our tests.
 const Post = require('../models/post');
+const User = require("../models/user");
 const server = require('../server');
 
 chai.should();
@@ -17,15 +18,28 @@ describe('Posts', function() {
     // Post that we'll use for testing purposes
     const newPost = {
         title: 'post title',
-        summary: 'post summary'
+        summary: 'post summary',
+        subreddit: 'subreddit'
     };
+    const user = {
+        username: 'poststest',
+        password: 'testposts'
+    };
+    before(function (done) {
+        agent.post('/users/sign-up')
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send(user).then(function (res) {
+            done();
+        }).catch(function (err) {
+            done(err);
+        });
+    });
+
     it("should create with valid attributes at POST /posts/new", function (done) {
         Post.estimatedDocumentCount().then(function (initialDocCount) {
-            agent
-            .post("/posts/new")
+            agent.post("/posts/new")
             .set("content-type", "application/x-www-form-urlencoded")
-            .send(newPost)
-            .then(function (res) {
+            .send(newPost).then(function (res) {
                 Post.estimatedDocumentCount().then(function (newDocCount) {
                     // Check that the database has one more post in it
                     expect(res).to.have.status(200);
@@ -45,7 +59,8 @@ describe('Posts', function() {
     after(async function () {
         try {
             await Post.findOneAndDelete({"title": newPost.title, "summary": newPost.summary});
-            console.log("Post deleted successfully");
+            await User.findOneAndDelete({ username: user.username });
+            console.log("Post & test user deleted successfully");
         } catch (error) {
             res.status(500).send(error);
         }
